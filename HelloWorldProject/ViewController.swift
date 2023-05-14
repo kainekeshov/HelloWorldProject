a29a75c76c133f06027e8c46978d4003a0bbcf10
Ruslan Kai <kainekeshov@gmail.com>
2023-05-14 17:32:48 +0600

    Realized dynamic Top and Bottom Views corner radiuses by buttons

--- HelloWorldProject/ViewController.swift
+++ HelloWorldProject/ViewController.swift
@@ -12,12 +12,36 @@ class ViewController: UIViewController {
     @IBOutlet var bottomView: UIView!
     @IBOutlet var topView: UIView!
     
+    let cornerRadiusModificator = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         bottomView.layer.cornerRadius = 10
         topView.layer.cornerRadius = 10
     }
 
-
+    @IBAction func plusButtonDidTapped() {
+        let modificator = CGFloat(cornerRadiusModificator)
+        bottomView.layer.cornerRadius += modificator
+        topView.layer.cornerRadius += modificator
+        if bottomView.layer.cornerRadius >= 60 {
+            resetButtonDidTapped()
+        }
+    }
+    
+    @IBAction func minusButtonDidTapped() {
+        let modificator = CGFloat(cornerRadiusModificator)
+        bottomView.layer.cornerRadius -= modificator
+        topView.layer.cornerRadius -= modificator
+        if bottomView.layer.cornerRadius <= 1 {
+            resetButtonDidTapped()
+        }
+    }
+    
+    @IBAction func resetButtonDidTapped() {
+        bottomView.layer.cornerRadius = 10
+        topView.layer.cornerRadius = 10
+    }
+    
 }
 
