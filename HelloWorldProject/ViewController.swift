8b760ef9b9a9dc12a500b227df4ffb12d4079282
Ruslan Kai <kainekeshov@gmail.com>
2023-05-14 18:12:16 +0600

    Added lights views

--- HelloWorldProject/ViewController.swift
+++ HelloWorldProject/ViewController.swift
@@ -8,40 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var bottomView: UIView!
-    @IBOutlet var topView: UIView!
     
-    let cornerRadiusModificator = 3
+     
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yelowLight: UIView!
+    @IBOutlet var greenLight: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bottomView.layer.cornerRadius = 10
-        topView.layer.cornerRadius = 10
+        redLight.layer.opacity = 0.3
+        yelowLight.layer.opacity = 0.3
+        greenLight.layer.opacity = 0.3
     }
+    
+//    func setDefaultSettings(to outlet: UIView, with) {
 
-    @IBAction func plusButtonDidTapped() {
-        let modificator = CGFloat(cornerRadiusModificator)
-        bottomView.layer.cornerRadius += modificator
-        topView.layer.cornerRadius += modificator
-        if bottomView.layer.cornerRadius >= 60 {
-            resetButtonDidTapped()
-        }
-    }
-    
-    @IBAction func minusButtonDidTapped() {
-        let modificator = CGFloat(cornerRadiusModificator)
-        bottomView.layer.cornerRadius -= modificator
-        topView.layer.cornerRadius -= modificator
-        if bottomView.layer.cornerRadius <= 1 {
-            resetButtonDidTapped()
-        }
-    }
-    
-    @IBAction func resetButtonDidTapped() {
-        bottomView.layer.cornerRadius = 10
-        topView.layer.cornerRadius = 10
-    }
     
 }
 
